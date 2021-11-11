23a7db57c1ede6539e82caf3c263ef90ea9ea54d
IGen <IGen@DESKTOP-7JNABSE>
2021-11-11 14:47:42 +0530

    Hi .. pls check

---
+++ Github.Practice/src/test/java/gitPractice/Practice3.java
@@ -0,0 +1,13 @@
+package gitPractice;
+
+import org.testng.annotations.Test;
+
+public class Practice3 {
+
+	@Test
+	
+	public void Practice3()
+	{
+		System.out.println("Hi there .. this is bu");
+	}
+}
